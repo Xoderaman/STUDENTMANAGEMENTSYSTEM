ce60ae18a85550e4c5a853d30326eae4bf4db3be
Amanhussain <112181574+Xoderaman@users.noreply.github.com>
2024-11-22 14:43:41 +0530

    Add files via upload

---
+++ DataFolder/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DemoCRUD.Models;
+
+
+namespace DemoCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor to pass DbContextOptions to the base DbContext class
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // DbSet to represent the "Students" table in the database
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoCRUD.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int EnrollmentId { get; set; }
+    }
+}
